944dce64130a64dc6353e9dee11af8bf4280c0c9
AR621 <60547627+AR621@users.noreply.github.com>
2020-06-21 14:57:15 +0100

    Comment update

--- source/card.h
+++ source/card.h
@@ -16,9 +16,9 @@ public:
 	~card();
 	int GetState();
 	int GetId();
-	void reveal();
-	void hide();
-	void keep();
+	void reveal();//sets state to 1
+	void hide();//sets state to 0
+	void keep();//sets state to 2
 	void SetId(int);
 	void setPosition(float,float);
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
